672acd00077fbb081b030dc3b19c2b1d8145ca9d
Abdellatif Zouliga <zuuliga@gmail.com>
2023-06-01 22:02:52 -0400

    some changes

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -40,11 +40,33 @@ describe('Find or get Elements by using different locators', () => {
         cy.get('button').should('contain','Login').click();
 
         
+    })
+
+    it('Finding element by traveling through DOM', () => {
+        // travel to find the login button: locate username box, go to parent form, then find login button
+        cy.get('input[name="username"]').parents('form').find('button').should('contain','Login').click();
+
+
+    })
+
+    it.only('Check different types of assertions', () => {
+        // Cypress itself bunbles assertions provided by Chai, Sinon and jQuery libraries
+
+        // Should Assertion
+        // locate login button with id and verify it has text login and class attribute value 'btn btn-primary'
+        cy.get('#wooden_spoon')               // locating the element
+        .should('contain','Login')            // assert element has a text 'Login'
+        .and('have.class','btn btn-primary')  // assert element class attribute's value is 'btn btn-primary'
+
+        // Expect Assertion
+        cy.get('#wooden_spoon').then((buttonElement) => {             // locate button element and create an object
+            expect(buttonElement).to.have.text('Login');              // assert element has the text 'Login'
+            expect(buttonElement).to.have.class('btn btn-primary');   // assert element class attribute's value is 'btn btn-primary'
+        })
+
+    })
 
     
-
-        
-    })
 
 
 })
